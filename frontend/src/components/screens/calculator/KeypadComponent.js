a226e24cdbf68766323459c51fb6cd1d373157ee
xeyzo <schneidergw@yahoo.com>
2023-01-12 21:44:21 -0800

    refactor styling

--- frontend/src/components/screens/calculator/KeypadComponent.js
+++ frontend/src/components/screens/calculator/KeypadComponent.js
@@ -5,11 +5,9 @@ class KeyPadComponent extends Component {
   render() {
     return (
       <div className="button">
-        <button name="(" onClick={e => this.props.onClick(e.target.name)}>(</button>
-        <button name="CE" onClick={e => this.props.onClick(e.target.name)}>CE</button>
-        <button name=")" onClick={e => this.props.onClick(e.target.name)}>)</button>
-        <button name="C" onClick={e => this.props.onClick(e.target.name)}>C</button>
-
+        <div className='row'>
+          <button name="CE" className="btn-calc col s6" onClick={e => this.props.onClick(e.target.name)}>CE</button>
+          <button name="C" className="btn-calc col s6" onClick={e => this.props.onClick(e.target.name)}>C</button>
         <button name="1" onClick={e => this.props.onClick(e.target.name)}>1</button>
         <button name="2" onClick={e => this.props.onClick(e.target.name)}>2</button>
         <button name="3" onClick={e => this.props.onClick(e.target.name)}>3</button>
@@ -29,6 +27,7 @@ class KeyPadComponent extends Component {
         <button name="0" onClick={e => this.props.onClick(e.target.name)}>0</button>
         <button name="=" onClick={e => this.props.onClick(e.target.name)}>=</button>
         <button name="/" onClick={e => this.props.onClick(e.target.name)}>/</button>
+      </div>
       </div>
     );
   }
